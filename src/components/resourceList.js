c25aac7d8b1c68ba63588c0cfee98dbed876495d
tester <aiforeveryoneai@gmail.com>
2020-08-21 15:50:48 +0500

    Update the project
    Converted App.js from a class based component to a functional component and added a class based component to send get requests to the selected end points provided in App.js

---
+++ src/components/resourceList.js
@@ -0,0 +1,25 @@
+import React from "react";
+import axios from "axios";
+class ResourceList extends React.Component {
+  state = { resources: [] };
+  async componentDidMount() {
+    const response = await axios.get(
+      `https://jsonplaceholder.typicode.com/${this.props.resource}`
+    );
+    this.setState({ resources: response.data });
+  }
+
+  async componentDidUpdate(prevProps) {
+    if (prevProps.resource !== this.props.resource) {
+      const response = await axios.get(
+        `https://jsonplaceholder.typicode.com/${this.props.resource}`
+      );
+      this.setState({ resources: response.data });
+    }
+  }
+  render() {
+    return this.state.resources.length;
+  }
+}
+
+export default ResourceList;
